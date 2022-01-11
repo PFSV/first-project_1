da87f94376089fe469c4eceb74da02bb3655a2d2
PFSV <96779838+PFSV@users.noreply.github.com>
2022-01-11 20:43:10 +0900

    Add files via upload
    백준

---
+++ 10590.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 20:21:47 2022
+
+@author: Doshite
+"""
+
+t = int(input())
+
+for i in range(t):
+    a,b = map(int,input().split())
+    print(a+b)
+    
