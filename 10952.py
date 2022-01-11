2c2abfbbbd033ddc40c0266067d814568105b5e9
PFSV <96779838+PFSV@users.noreply.github.com>
2022-01-12 07:18:20 +0900

    Add files via upload
    while

---
+++ 10952.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 07:13:09 2022
+
+@author: Doshite368
+"""
+
+
+# while 문 안에 넣어주기
+while True: 
+    a,b= map(int, input().split())
+    if a+b !=0:
+        print(a+b)
+    else:
+        break
+    
